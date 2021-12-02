ebd41fd52262c8a00bca8334ab7fdedeee0335ed
Sam Gardner <samcgardner@gmail.com>
2021-12-02 12:11:59 +0000

    Add windowed implementation

--- 1/main.go
+++ 1/main.go
@@ -9,6 +9,11 @@ import (
 )
 
 func main() {
+	partOne()
+	partTwo(3)
+}
+
+func partOne() {
 	file, err := os.Open("./input.txt")
 	if err != nil {
 		panic(err)
@@ -28,5 +33,30 @@ func main() {
 		}
 		previous = current
 	}
-	fmt.Printf("%d\n", total)
+	fmt.Printf("Solution to part one: %d\n", total)
+}
+
+func partTwo(window int) {
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	queue := make([]int, window)
+	total := -window
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		current, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			panic(err)
+		}
+		previous := queue[0]
+		queue = queue[1:]
+		if current > previous {
+			total++
+		}
+		queue = append(queue, current)
+	}
+	fmt.Printf("Solution to part two: %d\n", total)
 }
